42219a6136242ddfd66f57521f2e3f141df00d7c
manuels9 <60626546+manuels9@users.noreply.github.com>
2020-03-16 11:52:20 -0500

    Update pw.js
    Add valid password alert

--- exercise1/pw.js
+++ exercise1/pw.js
@@ -1,7 +1,7 @@
 function validate()
 {
-  password1 = document.getElementById("pw1").value;
-  password2 = document.getElementById("pw2").value;
+  let password1 = document.getElementById("pw1").value;
+  let password2 = document.getElementById("pw2").value;
   if(password1.length < 8)
   {
     alert("Error: invalid password length!");
@@ -9,5 +9,9 @@ function validate()
   else if (password1 != password2)
   {
     alert("Error: passwords do not match!");
+  }
+  else
+  {
+    alert("Valid password!");
   }
 }
