956f5e84e3736637f2f1e084bd792f15e09c0249
manuels9 <60626546+manuels9@users.noreply.github.com>
2020-03-15 19:53:38 -0500

    Exercise 2, 3 update
    Fix bug and update images in Exercise 2, Update formatting in Exercise 3

--- exercise2/ss.js
+++ exercise2/ss.js
@@ -11,13 +11,17 @@ function previous()
     imgNum = 5;
   }
   currentImage.value = imgNum.toString();
-  currentImage.src = "images/img" + imgNum + ".jpg";
+  currentImage.src = "images/img" + imgNum + ".gif";
 }
 
 function next()
 {
   let currentImage = document.getElementById("i1");
   let imgNum = parseInt(document.getElementById("i1").value)
+  if(isNaN(imgNum))//check for initial NaN value for i1
+  {
+    imgNum = 1;
+  }
   if(imgNum < 5)
   {
     imgNum = imgNum + 1;
@@ -27,6 +31,5 @@ function next()
     imgNum = 1;
   }
   currentImage.value = imgNum.toString();
-  currentImage.src = "images/img" + imgNum + ".jpg";
-
+  currentImage.src = "images/img" + imgNum + ".gif";
 }
